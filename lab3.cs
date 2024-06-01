24cc687ff39c6bfc5b5dc0671de433309d9c2841
Erik-Certain <167526282+Erik-Certain@users.noreply.github.com>
2024-06-01 03:35:54 +0000

    .

--- lab3.cs
+++ lab3.cs
@@ -5,33 +5,27 @@ class Program
 {
     static void Main()
     {
-        // Создание героя с арсеналом вооружения
         var hero = new Hero(new List<Equipment>
         {
-            new Armor("Chainmail"), // Кольчуга
-            new Weapon("Sword"),    // Меч
-            new Armor("Helmet"),    // Шлем
-            new Weapon("Bow")       // Лук
+            new Armor("Chainmail"),
+            new Weapon("Sword"),
+            new Armor("Helmet"),
+            new Weapon("Bow")
         });
-
-        // Вывод вооружения героя в консоль
+      
         hero.DisplayArsenal();
     }
 }
 
-// Класс героя
 public class Hero
 {
-    // Коллекция вооружения героя
     public List<Equipment> Arsenal { get; private set; }
 
-    // Конструктор
     public Hero(List<Equipment> arsenal)
     {
         Arsenal = arsenal;
     }
-
-    // Метод для отображения арсенала
+  
     public void DisplayArsenal()
     {
         Console.WriteLine("Arsenal of the Hero:");
@@ -42,20 +36,16 @@ public class Hero
     }
 }
 
-// Абстрактный класс снаряжения
 public abstract class Equipment
 {
-    // Название снаряжения
     public string Name { get; private set; }
 
-    // Конструктор
     protected Equipment(string name)
     {
         Name = name;
     }
 }
 
-// Класс доспехов
 public class Armor : Equipment
 {
     public Armor(string name) : base(name)
@@ -63,7 +53,6 @@ public class Armor : Equipment
     }
 }
 
-// Класс оружия
 public class Weapon : Equipment
 {
     public Weapon(string name) : base(name)
